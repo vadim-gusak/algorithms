be05f9970488b13a990b165828940473c0125c78
Vadim Gusak <gusakvd@gmail.com>
2023-01-27 16:57:15 +1000

    Update select_sort.py

--- algorithms/select_sort.py
+++ algorithms/select_sort.py
@@ -6,14 +6,17 @@ def sort_by_score(users: list[dict], reverse=False) -> list:
     for index_1 in range(users_len - 1):
         for index_2 in range(index_1 + 1, users_len):
 
+            index_temporary = index_1
+
             if not reverse and \
                     users[index_2]['score'] > users[index_1]['score']:
-                users[index_1], users[index_2] = \
-                    users[index_2], users[index_1]
+                index_temporary = index_2
 
             if reverse and \
                     users[index_2]['score'] < users[index_1]['score']:
-                users[index_1], users[index_2] = \
-                    users[index_2], users[index_1]
+                index_temporary = index_2
+
+            users[index_1], users[index_temporary] = \
+                users[index_temporary], users[index_1]
 
     return users
